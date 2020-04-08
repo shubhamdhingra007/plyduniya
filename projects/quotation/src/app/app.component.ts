74b0dc968dc917ef367341537f5ff69da990cc9c
Shubham Dhingra <shubham.dhingra@moveinsync.com>
2020-04-08 12:26:40 +0530

    initial commit

---
+++ projects/quotation/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quotation-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'quotation';
+}
