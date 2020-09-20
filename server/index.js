9e962dd8224f85e074d392f843fb500ac51cf2c8
Shubham Dhingra <shubham.dhingra@moveinsync.com>
2020-09-20 11:02:13 +0530

    wip

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require("path");
+const app = express()
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3003, () => console.log('Listening on port 3003!'))
